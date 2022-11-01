b34547c6d95acc1a91e15ecf485b778135e2e4dc
DESKTOP-8JNNUII\MD SOHORAB HOSSAIN <mdsohorab2229@gmail.com>
2022-11-02 03:10:58 +0600

    backend template and auth breeze install

---
+++ resources/views/backend/includes/script.blade.php
@@ -0,0 +1,3 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="{{ asset('backend/js/scripts.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Frontend\FrontendController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(FrontendController::class)->group(function (){
-    Route::get('/','index')->name('front.index');
-    Route::get('/single-post','single')->name('front.single');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
